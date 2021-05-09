b52f2b7b5b35e1c27081feac0d1995f799c525d8
Lazaro Hurtado <hurtadolazaro12@gmail.com>
2021-05-09 16:43:31 -0400

    different invalid blocks response struct

--- rpc/src/responses/chains/blocks/invalid_blocks_in_chain.rs
+++ rpc/src/responses/chains/blocks/invalid_blocks_in_chain.rs
@@ -1,13 +1,25 @@
 use crate::errors::ParseError;
 use crate::responses::{json_array, Response};
-use crate::types::{ResponseError, Unistring};
+use crate::types::Unistring;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize)]
+pub struct InvalidBlockError {
+	pub kind: String,
+	pub id: String,
+	pub invalid_block: Unistring,
+	pub error: String,
+	#[serde(flatten)]
+	pub extra_error_info: HashMap<String, Value>,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct InvalidBlock {
-	block: Unistring,
-	level: i32,
-	errors: ResponseError,
+	pub block: Unistring,
+	pub level: i32,
+	pub errors: InvalidBlockError,
 }
 
 pub struct InvalidBlocksInChainResponse {
@@ -41,31 +53,33 @@ mod test {
 
 	#[test]
 	fn get_invalid_blocks_in_chain_from_response_ok() {
-		let mock_block_id = "blockId1";
-		let mock_block_level = 1;
+		let mock_block = "blockId1";
+		let mock_level = 1;
 
-		let mock_block_error = r#"{
-			"title":"Identity keys mismatch",
-			"description":"The current identity file has non-matching keys (secret key/ public key pair is not valid)",
-			"type":"object",
-			"properties":{
-					"kind":{
-							"type":"string",
-							"enum":[
-									"permanent"
-							]
-					}
-			},
-			"required":[
-					"kind"
-			],
-			"additionalProperties":false
-		}"#;
-		let stripped_mock_block_error = mock_block_error.replace('\n', "").replace('\t', "");
+		let mock_kind = "permanent";
+		let mock_id = "validator.invalid_block";
+		let mock_invalid_block_hash = "blockId1";
+		let mock_error = "cannot_parse_operation";
+		let mock_operation = "operationHash1";
+
+		let mock_error_response = format!(
+			r#"{{
+				"kind":"{}",
+				"id":"{}",
+				"invalid_block":"{}",
+				"error": "{}",
+				"operation": "{}"
+			}}"#,
+			mock_kind, mock_id, mock_invalid_block_hash, mock_error, mock_operation
+		);
 
 		let mock_response = format!(
-			r#"[{{"block":"{}", "level":{}, "errors":{}}}]"#,
-			mock_block_id, mock_block_level, mock_block_error
+			r#"[{{
+				"block":"{}",
+				"level":{},
+				"errors": {}
+			}}]"#,
+			mock_block, mock_level, mock_error_response
 		);
 
 		let invalid_blocks_response = InvalidBlocksInChainResponse::from_response_str(&mock_response);
@@ -75,20 +89,33 @@ mod test {
 		assert!(invalid_blocks.len() == 1);
 
 		let invalid_block = invalid_blocks.pop().unwrap();
-		assert!(invalid_block.block.get_string() == mock_block_id);
-		assert!(invalid_block.level == mock_block_level);
-		assert_eq!(invalid_block.errors.get_string(), stripped_mock_block_error);
+		assert!(invalid_block.block.get_string() == mock_block);
+		assert!(invalid_block.level == mock_level);
+
+		let invalid_block_errors = invalid_block.errors;
+		let invalid_block_errors_operation = invalid_block_errors
+			.extra_error_info
+			.get("operation")
+			.unwrap();
+		assert!(invalid_block_errors.kind == mock_kind);
+		assert!(invalid_block_errors.id == mock_id);
+		assert!(invalid_block_errors.invalid_block.get_string() == mock_invalid_block_hash);
+		assert!(invalid_block_errors.error == mock_error);
+		assert!(invalid_block_errors_operation == mock_operation);
 	}
 
 	#[test]
 	fn get_invalid_blocks_in_chain_from_malformed_response_fails() {
-		let mock_block_id = "blockId1";
-		let mock_block_level = 2;
+		let mock_block = "blockId2";
+		let mock_level = 2;
 
-		// Mock response without errors field
+		// Mock response without errors field, which is required for all type of errors
 		let mock_response = format!(
-			r#"[{{"block":"{}", "level":{}}}]"#,
-			mock_block_id, mock_block_level
+			r#"[{{
+				"block":"{}",
+				"level":{},
+			}}]"#,
+			mock_block, mock_level
 		);
 
 		let invalid_blocks_response = InvalidBlocksInChainResponse::from_response_str(&mock_response);
