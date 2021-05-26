89646fb0052065b8131a0162cdb7cc43559c8946
astherath <farceriv@gmail.com>
2021-05-26 11:42:45 -0400

    added unit tests to params in chain

--- rpc/src/commands/chains/blocks/get_blocks_in_chain.rs
+++ rpc/src/commands/chains/blocks/get_blocks_in_chain.rs
@@ -19,33 +19,6 @@ pub struct GetBlocksInChain {
     params: Option<GetBlocksInChainParameters>,
 }
 
-#[derive(Debug)]
-struct GetBlocksInChainParameters {
-    length: Option<u32>,
-    head: Option<BlockHash>,
-    min_date: Option<NaiveDateTime>,
-}
-
-impl GetBlocksInChainParameters {
-    fn to_url_query_string(&self) -> String {
-        let mut query_pairs = Vec::new();
-
-        if let Some(length) = &self.length {
-            query_pairs.push(("length", length.to_string()));
-        }
-        if let Some(head) = &self.head {
-            query_pairs.push(("head", head.to_string()));
-        }
-        if let Some(min_date) = &self.min_date {
-            query_pairs.push(("min_date", min_date.timestamp().to_string()));
-        }
-
-        let query_params = query_pairs.iter().map(|x| (x.0, x.1.as_str())).collect();
-
-        querystring::stringify(query_params)
-    }
-}
-
 impl GetBlocksInChain {
     pub fn with_default_params(chain_id: Chain) -> Self {
         Self {
@@ -87,5 +60,116 @@ impl RpcClientCommand for GetBlocksInChain {
 
     fn get_http_method(&self) -> reqwest::Method {
         reqwest::Method::GET
+    }
+}
+
+#[derive(Debug)]
+struct GetBlocksInChainParameters {
+    length: Option<u32>,
+    head: Option<BlockHash>,
+    min_date: Option<NaiveDateTime>,
+}
+
+impl GetBlocksInChainParameters {
+    fn to_url_query_string(&self) -> String {
+        let mut query_pairs = Vec::new();
+
+        if let Some(length) = &self.length {
+            query_pairs.push(("length", length.to_string()));
+        }
+        if let Some(head) = &self.head {
+            query_pairs.push(("head", head.to_string()));
+        }
+        if let Some(min_date) = &self.min_date {
+            query_pairs.push(("min_date", min_date.timestamp().to_string()));
+        }
+
+        let query_params = query_pairs.iter().map(|x| (x.0, x.1.as_str())).collect();
+
+        querystring::stringify(query_params)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn all_params_some_to_string_ok() {
+        let length = 10;
+        let head = "head".to_string();
+        let min_date = NaiveDateTime::from_timestamp(0, 0);
+
+        let correct_query_string = format!(
+            "length={}&head={}&min_date={}&",
+            &length,
+            &head,
+            &min_date.timestamp()
+        );
+
+        let params = GetBlocksInChainParameters {
+            length: Some(length),
+            head: Some(head),
+            min_date: Some(min_date),
+        };
+
+        assert_eq!(correct_query_string, params.to_url_query_string());
+    }
+
+    #[test]
+    fn all_params_none_to_string_ok() {
+        let correct_query_string = "".to_string();
+
+        let params = GetBlocksInChainParameters {
+            length: None,
+            head: None,
+            min_date: None,
+        };
+
+        assert_eq!(correct_query_string, params.to_url_query_string());
+    }
+
+    #[test]
+    fn only_length_some_ok() {
+        let length = 10;
+
+        let correct_query_string = format!("length={}&", &length,);
+
+        let params = GetBlocksInChainParameters {
+            length: Some(length),
+            head: None,
+            min_date: None,
+        };
+
+        assert_eq!(correct_query_string, params.to_url_query_string());
+    }
+    #[test]
+    fn only_head_some_ok() {
+        let head = "head".to_string();
+
+        let correct_query_string = format!("head={}&", &head,);
+
+        let params = GetBlocksInChainParameters {
+            length: None,
+            head: Some(head),
+            min_date: None,
+        };
+
+        assert_eq!(correct_query_string, params.to_url_query_string());
+    }
+
+    #[test]
+    fn only_min_date_some_ok() {
+        let min_date = NaiveDateTime::from_timestamp(0, 0);
+
+        let correct_query_string = format!("min_date={}&", &min_date.timestamp());
+
+        let params = GetBlocksInChainParameters {
+            length: None,
+            head: None,
+            min_date: Some(min_date),
+        };
+
+        assert_eq!(correct_query_string, params.to_url_query_string());
     }
 }
